5ecef3105aa6606abddd8f192db3e846db551c44
Shamsuddin I W <taheeronline@outlook.com>
2026-02-14 12:20:28 +0530

    QuickGrid Demoimplemented for generic class. Need to update with database real data

---
+++ ApartmentManagement.UI/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace ApartmentManagement.UI.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateOnly DateOfBirth{ get; set; }=DateOnly.FromDateTime(DateTime.Now);
+    }
+}
