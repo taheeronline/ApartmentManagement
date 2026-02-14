f27e3982201e7539b052b3beaeea73f241c1879e
Shamsuddin I W <taheeronline@outlook.com>
2026-02-14 12:53:57 +0530

    Updated razorview names as to implement apartment grid functionality separately

--- ApartmentManagement.UI/Models/Person.cs
+++ ApartmentManagement.UI/Models/Person.cs
@@ -3,7 +3,7 @@
     public class Person
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public DateOnly DateOfBirth{ get; set; }=DateOnly.FromDateTime(DateTime.Now);
     }
 }
